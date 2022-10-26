a2e1777f2efb0e104c98fef55da037af3ed35b2b
Lidhya <lidhyajeejan@gmail.com>
2022-10-26 22:36:50 +0530

    Admin login completed

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -3,7 +3,7 @@ const db = require('../db/authHelpers');
 module.exports.registerAdmin = function (req, res) {
     try {
         const data = req.body
-        const {username, password, email, organisation}=data
+        const { username, password, email, organisation } = data
         if (username && password && email && organisation) {
             db.registerAdmin(data).then((response) => {
                 res.json({ status: 200, message: "Registration success" });
@@ -11,21 +11,25 @@ module.exports.registerAdmin = function (req, res) {
                 res.json({ status: 500, message: error.message })
             })
         }
-        else  res.json({ status: 500, message: "Insufficient data" })
+        else res.json({ status: 500, message: "Insufficient data" })
     } catch (error) {
         res.json({ status: 501, message: error.message });
     }
 }
 
 module.exports.adminLogin = function (req, res) {
-    //     try {
-    //         db.Login(req.body).then((response) => {
-    //             res.json({ status: 200, message: "Login success" });
-    //         }).catch((error) => {
-    //             res.json({ status: 500, message: error.message })
-    //         })
-    //     } catch (error) {
-    //         console.log(error);
-    //         res.json({ status: 501, message: error.message });
-    //     }
+    try {
+        const data = req.body
+        const { username, password } = data
+        if (username && password) {
+            db.Login(data).then((response) => {
+                res.json({ status: 200, message: "Login success" });
+            }).catch((error) => {
+                res.json({ status: 500, message: error.message })
+            })
+        }
+        else res.json({ status: 500, message: "Insufficient data" })
+    } catch (error) {
+        res.json({ status: 501, message: error.message });
+    }
 }
