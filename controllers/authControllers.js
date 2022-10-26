0252922e7c1d73dffcf846d342d6872321895f96
Lidhya <lidhyajeejan@gmail.com>
2022-10-26 16:10:42 +0530

    signup arguments checked

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -2,14 +2,17 @@ const db = require('../db/authHelpers');
 
 module.exports.registerAdmin = function (req, res) {
     try {
-        db.registerAdmin(req.body).then((response) => {
-            res.json({ status: 200, message: "Registration success" });
-        }).catch((error) => {
-            res.json({ status: 500, message: error.message })
-        })
-
+        const data = req.body
+        const {username, password, email, organisation}=data
+        if (username && password && email && organisation) {
+            db.registerAdmin(data).then((response) => {
+                res.json({ status: 200, message: "Registration success" });
+            }).catch((error) => {
+                res.json({ status: 500, message: error.message })
+            })
+        }
+        else  res.json({ status: 500, message: "Insufficient data" })
     } catch (error) {
-        console.log(error);
         res.json({ status: 501, message: error.message });
     }
 }
