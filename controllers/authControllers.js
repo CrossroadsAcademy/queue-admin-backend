9e5bfa6f463f77f8fdf74fa707c620be86f648d7
Faisal K <faisalkkn9946@gmail.com>
2022-10-31 12:19:09 +0530

    Forgot password mail sent, otp is pending

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,5 +1,5 @@
 const db = require('../db/authHelpers');
-const { validateRegister, validateLogin } = require('../validations/authValidators');
+const { validateRegister, validateLogin, forgotPassword } = require('../validations/authValidators');
 
 module.exports.registerAdmin = function (req, res) {
     try {
@@ -28,6 +28,24 @@ module.exports.adminLogin = function (req, res) {
         }).catch((error) => {
             res.json({ status: 500, message: error.message })
         })
+    } catch (error) {
+        res.json({ status: 501, message: error.message });
+    }
+}
+
+
+module.exports.forgotPassword = function (req, res) {
+    try {
+        const { error, value } = forgotPassword(req.body)
+        if (error) {
+            return res.json({ status: 422, errors: error.details })
+        }
+        db.ForgotPassword(value).then((response) => {
+            res.json({ status: 200, message: "mail sended" });
+        }).catch((error) => {
+            res.json({ status: 500, message: error.message })
+        })
+
     } catch (error) {
         res.json({ status: 501, message: error.message });
     }
